72d0e001bee9e2f2ca1fdb15a23b8a5701450de5
Paradox <jhe@nexient.com>
2019-11-12 13:32:30 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+//configurations for babel loader
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+        ],
+    },
+    plugins: [htmlPlugin]
+};
